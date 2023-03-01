62cbbc33908a340afae7595d00825888af1972df
Dashrath <dashrath.angular@gmail.com>
2023-03-01 08:49:25 +0530

    slicevssplice

---
+++ SlicevsSplice.js
@@ -0,0 +1,12 @@
+let myArr = [1,2,3,4,5,6,7,8,9];
+
+// 1. slice can't change original array
+// 2. slice method use only 2 argument slice(start, End)
+// console.log( myArr.slice(3,5));
+// console.log(myArr);
+
+
+// 1. splice can change original array
+// 2. splice method use more than 2 argument splice(start, deleteCount, item1, item2, itemN)
+// console.log( myArr.splice(3,5));
+// console.log(myArr);
